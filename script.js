1e0ebf9e6a2374121a3703fe0cd07b8238daf434
noboKumar <nobokumar227@gmail.com>
2025-03-14 01:16:06 +0600

    add video fetch

--- script.js
+++ script.js
@@ -17,4 +17,49 @@ const displayCategory = (categories) => {
   }
 };
 
+const fetchVideo = async () => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/phero-tube/videos#"
+  );
+  const data = await response.json();
+  loadVideo(data.videos);
+};
+const loadVideo = (videos) => {
+  const videoContainer = document.getElementById("video-container");
+
+  for (const video of videos) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+     <div class="card bg-base-100 shadow-sm cursor-pointer">
+        <figure>
+          <img class="w-full h-56 object-cover"
+            src="${video.thumbnail}"
+            alt="video thumbnail"
+          />
+        </figure>
+        <div class="card-body">
+          <div class="flex gap-3">
+            <div>
+              <div class="avatar">
+                <div class="w-12 rounded-full">
+                  <img src="${video.authors[0].profile_picture}" />
+                </div>
+              </div>
+            </div>
+            <div class="grid gap-1">
+              <h2 class="card-title font-bold">
+               ${video.title}
+              </h2>
+              <p class="text-sm text-gray-500 font-medium flex gap-1">${video.authors[0].profile_name}<img class="w-5" src="./assets/verified.png" alt="verified badge"></p>
+              <p class="text-sm text-gray-500 font-medium">${video.others.views} views</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    videoContainer.append(div);
+  }
+};
+
+fetchVideo();
 fetchCatagory();
